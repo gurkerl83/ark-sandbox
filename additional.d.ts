c928b3a11ba8371373cf4afb673faa0c379799b1
Markus Gritsch <gurkerl83@googlemail.com>
2024-03-28 12:10:37 +0100

    Add general project setup

---
+++ additional.d.ts
@@ -0,0 +1,6 @@
+declare module 'lucide-react/dist/esm/icons/*' {
+  import type { FC, SVGProps } from 'react';
+
+  const IconComponent: FC<SVGProps<SVGSVGElement>>;
+  export default IconComponent;
+}
